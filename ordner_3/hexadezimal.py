005f9aed6d007b976096bfb41afcb9cd1148fe02
ahielscher <anne.hielscher@stud.schulegl.ch>
2021-09-22 09:58:19 +0300

    add hexadezimal-program from tigerjython

---
+++ ordner_3/hexadezimal.py
@@ -0,0 +1,10 @@
+
+h=[]
+v=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+y=input(" gib die selbe zahl z nochmal ein") 
+print"Deine Zahl im Hexadezimalsystem ist:"       
+while y >=16:
+        y/=16
+        
+        print y
+        
